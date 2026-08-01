753aa0127f31e237aa295bec301a725f16420d20
Naresh Chauhan <nareshsingh0170@gmail.com>
2026-08-01 17:05:37 +0530

    Leavel 4 Done

---
+++ Level-04/find-the-largest-of-tow-number.js
@@ -0,0 +1,22 @@
+// Find the Largest of Two Numbers
+let num1 = 100;
+let num2 = 200;
+
+if (num1 > num2) {
+  console.log(`The largest number is: ${num1}`);
+} else {
+  console.log(`The largest number is: ${num2}`);
+}
+
+// Find the largest of three numbers
+let a = 100;
+let b = 20;
+let c = 30;
+
+if (a > b && a > c) {
+  console.log(`The largest number is: ${a}`);
+} else if (b > a && b > c) {
+  console.log(`The largest number is: ${b}`);
+} else {
+  console.log(`The largest number is: ${c}`);
+}
