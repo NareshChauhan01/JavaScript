31e2bd67e91a38d3db1405168fd282f906bafc82
Naresh Chauhan <nareshsingh0170@gmail.com>
2026-08-01 14:30:59 +0530

    Level 2 Done

---
+++ Level-01/printhelloworld.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+console.log("Pattern Print");
+
+console.log("-----");
+console.log("*");
+console.log("**");
+console.log("***");
+console.log("****");
+console.log("*****");
+
+// 
+console.log("-----");
+console.log("N");
+console.log("Na");
+console.log("Nar");
+console.log("Nare");
+console.log("Nares");
+console.log("Naresh");
---
+++ Level-02/addtwonumber.js
@@ -0,0 +1,8 @@
+const number1 = 10;
+const number2 = 20;
+
+const result = number1 + number2;
+
+console.log("the sum of two number", result);
+// Template literals
+console.log(`the sum of two number: ${result}`);
